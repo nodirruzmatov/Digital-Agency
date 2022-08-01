f1d6a3584c04c09f6c7de41804d9df537fd36b89
nodirruzmatov <nodir7ruzmatov@gmail.com>
2022-08-01 19:08:21 +0300

    feat: add product section

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,11 +1,13 @@
 import "./main.scss";
 import Hiro from "../Hiro"
 import About from "../About"
+import Product from "../Product"
 
 const index = () => {
   return<>
     <Hiro />
     <About />
+    <Product />
   </>
 };
 
---
+++ src/components/Product/index.jsx
@@ -0,0 +1,19 @@
+import "./main.scss";
+
+const index = () => {
+  return<>
+  
+    <section className="product">
+      <div className="product__container container">
+
+        <h2 className="product__heading">Ürününüz veya hizmetiniz için en iyisini birlikte yapalım.</h2>
+        <p className="product__desc">360° dijital marka danışmanlığı, web tasarım ve reklam ajansıdır. İşletmelerin ihtiyaçlarına uygun web tasarım ve web yazılım hizmetleri sunmaktayız.</p>
+        <button className="product__btn">HEMEN TEKLİF ALIN</button>
+
+      </div>
+    </section>
+  
+  </>
+};
+
+export default index;
